163470bdc4ef4d81618df7c339dd84c00c6d2d3d
hungcao0402 <hungcao0402@gmail.com>
2023-12-22 21:21:24 +0000

    init

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@
 #     done
 # done
 
-CUDA_VISIBLE_DEVICES=3,0 python examples/train.py -b 128 -a resnet50 -d market1501 --iters 200 --eps 0.45 --num-instances 16 --pooling-type gem --memorybank CMhybrid_v2 --epochs 60  --negative_sample "$neg"  --positive_sample "$pos" --logs-dir "examples/logs/market1501/{$pos}_{$neg}"
+CUDA_VISIBLE_DEVICES=0,1,2,3 python examples/train.py -b 256 -a resnet50 -d market1501 --iters 200 --eps 0.45 --num-instances 16 --pooling-type avg --memorybank CMhybrid_v2 --epochs 60  --negative_sample hard  --positive_sample hard --logs-dir "examples/logs/market1501/t"
